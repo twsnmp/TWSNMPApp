b44a61438aaf3981b438673f7f307078297e2058
twsnmp <ymi@twise.co.jp>
2020-10-04 16:17:26 +0900

    Initial Commit

---
+++ TWSNMPApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TWSNMPApp
+//
+//  Created by 山居正幸 on 2020/10/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TWSNMPApp/TWSNMPAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TWSNMPAppApp.swift
+//  TWSNMPApp
+//
+//  Created by 山居正幸 on 2020/10/04.
+//
+
+import SwiftUI
+
+@main
+struct TWSNMPAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
